e5918549b6c02817306659c83d0718c5a6bd6e7d
karolmatuszak7@gmail.com <56365605+Kar977@users.noreply.github.com>
2026-06-17 16:38:07 +0200

    feat: add shadcn/ui base components and cn helper

---
+++ apps/web/src/shared/ui/index.ts
@@ -0,0 +1,39 @@
+export { Button, buttonVariants } from "./button";
+export { Input } from "./input";
+export { Label } from "./label";
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
+export { Badge, badgeVariants } from "./badge";
+export { Separator } from "./separator";
+export {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
+export {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./dialog";
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
+export {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
+export { Avatar, AvatarFallback, AvatarImage } from "./avatar";
